a9fdc271933c97d805ecde243d5097c90e9bbfd5
dannychou <danny75@gmail.com>
2019-11-07 11:47:01 +0800

    Spring Boot以JSP方式呈現VIEW層介紹

--- demo/src/main/java/com/example/demo/UserController.java
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * UserController
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UserController {
 
-    @RequestMapping("/MyFirstPage")
-    public String greeting(@RequestParam(value="title", required=false, defaultValue="鐵人賽 加油!") String title, Model model) {
-        model.addAttribute("name", title);
+    private String message = "鐵人賽第七天加油!!!";
+
+    @GetMapping("/")
+    public String index(Map<String, Object> model) {
+        model.put("message", this.message);
         return "index";
     }  
-    
+
 }
