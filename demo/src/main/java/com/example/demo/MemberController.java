eb5739daf7c09b86ddc20016fb4326d6ad263141
dannychou <danny75@gmail.com>
2019-11-08 12:31:55 +0800

    Day 11-Spring Boot-如何載入靜態資源-使用thymeleaf模板引擎

--- demo/src/main/java/com/example/demo/MemberController.java
+++ demo/src/main/java/com/example/demo/MemberController.java
@@ -1,29 +1,26 @@
 package com.example.demo;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.Model.MemberAccount;
 import com.example.Service.MemberService;
 
-@RestController
+@Controller
 public class MemberController {
-	//透過 @RequestMapping 指定從/會被對應到此hello()方法
+	//透過 @RequestMapping 指定從/會被對應到此addMemberPage()方法
 	@Autowired
 	MemberAccount memberAccount;
 	
 	@Autowired
 	MemberService memberService;
-    @RequestMapping("/")
-    public String hello(){
-    	memberAccount = new MemberAccount();
-    	memberAccount.setPassword("12345678");
-    	memberAccount.setEmail("test@gmail.com");
-    	memberAccount.setCellphone("0912345789");
-    	memberAccount.setAddress("台北市");
-    	memberService.addMember(memberAccount);
-        return "新增會員成功";
-    }
+	@GetMapping("/addMemberPage")
+    public String addMemberPage(){
+//    	memberAccount = new MemberAccount();
+//    	memberService.addMember(memberAccount);
+
+        return "addMemberPage";
+	}
+	
 }
